70463445713cb2a03deb3ed58c7b79a41b1edccf
Egwu Vincent Oko <egwuoko76@gmail.com>
2025-12-14 14:43:05 +0100

    Fixed validation errors

--- providers.tf
+++ providers.tf
@@ -1,16 +1,6 @@
 # Determine the current Terraform workspace
 terraform {
   required_version = ">= 1.6"
-}
-
-locals {
-  current_env = terraform.workspace != "default" ? terraform.workspace : "dev"
-  tfvars_file = "envs/${local.current_env}/terraform.tfvars"
-}
-
-# Load variables automatically from the workspace-specific tfvars file
-terraform {
-  required_version = ">= 1.6"
 
   required_providers {
     azurerm = {
@@ -18,6 +8,11 @@ terraform {
       version = "~> 4.0"
     }
   }
+}
+
+locals {
+  current_env = terraform.workspace != "default" ? terraform.workspace : "dev"
+  tfvars_file = "envs/${local.current_env}/terraform.tfvars"
 }
 
 provider "azurerm" {
